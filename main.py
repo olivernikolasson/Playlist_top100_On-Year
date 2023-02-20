330c72b23fdf9e32fd5e18463ff6567b71e4b406
Oliver Nikolasson <125803388+olivernikolasson@users.noreply.github.com>
2023-02-20 13:07:06 +0100

    Create main.py
    when you first run the script a webpage will be opend fo you to Authorize the account. when taken to the "http://example.com" site, copy the entire URL and paste it into the terminal where it asks for the URL. 
    This will get the neccisery tokens and user_ids.
    
    Usually you only need to do this once. After that, enter the date in the format shown and press enter. It will now create a playlist and add all the songs to that playlist using the URI

---
+++ main.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+import spotipy
+
+
+
+CLIENT_ID = "SPORTIFY CLIENT ID FROM SPORIFY DEVELOPER PORTAL"
+CLIENT_SECRET = "SPORTIFY SERCRET ID FROM SPORIFY DEVELOPER PORTAL"
+
+
+oauth_object = spotipy.SpotifyOAuth(client_id=CLIENT_ID,
+                                    client_secret=CLIENT_SECRET,
+                                    redirect_uri="http://example.com",
+                                    show_dialog=True,
+                                    cache_path="token.txt",
+                                    scope="playlist-modify-private"
+                                    )
+
+sp = spotipy.Spotify(auth_manager=oauth_object)
+
+user_id = sp.current_user()["id"]
+
+# user input for top 100 year
+year = input("Which year would you like to create a top 100 ? \nYYYY-MM-DD\n")
+# gets url for that year
+URL = f"https://www.billboard.com/charts/hot-100/{year}"
+
+# gets data
+response = requests.get(URL)
+top_html = response.text
+
+user_id = sp.current_user()["id"]
+# parses the site.
+soup = BeautifulSoup(top_html, "html.parser")
+# to find the song name with css/html selector using beautifulsoup
+songs_list = soup.select("li ul li h3")
+
+# adds song to list and removes all newlines since the H3 element is full of them
+song_names = [song.get_text(strip=True) for song in songs_list]
+
+# creates playlist with the enterd date in the year
+my_playlist = sp.user_playlist_create(user=f"{user_id}", name=f"{year}: Top Tracks", public=False,
+                                      description="OPTIONAL")
+# song uri/tracks list
+song_uri = []
+# loops and gets all URIs
+for i in song_names:
+    track = sp.search(q=' track:' + i, type='track')
+    track_uri = (track["tracks"]["items"][0]["uri"])
+    song_uri.append(track_uri)
+
+ # creates playlist from song_uri list. 
+sp.playlist_add_items(playlist_id=my_playlist["id"],items=song_uri)
+
+
+
